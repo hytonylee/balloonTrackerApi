93ccf9afa9f7914322903122c5630c06e89c7df5
Tony Lee <hy.tony.lee@gmail.com>
2021-01-20 00:03:02 -0800

    create placeholder request

--- server.js
+++ server.js
@@ -6,9 +6,44 @@ dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.get('/api/v1/balloons', (req, res)=> {
+  res.status(200).json({
+    success: true,
+    message: 'Show all balloons'
+  })
+});
 
-app.listenerCount(
+app.post('/api/v1/balloons', (req, res)=> {
+  res.status(200).json({
+    success: true,
+    message: 'Create a balloon'
+  })
+});
+
+app.put('/api/v1/balloons/:id', (req, res)=> {
+  res.status(200).json({
+    success: true,
+    message: `update balloon with id: ${req.params.id}`
+  })
+});
+
+app.get('/api/v1/balloons/:id', (req, res)=> {
+  res.status(200).json({
+    success: true,
+    message: `Get a balloon with id of ${req.params.id}`
+  })
+});
+
+app.delete('/api/v1/balloons/:id', (req, res)=> {
+  res.status(200).json({
+    success: true,
+    message: `Delete a balloon with id of ${req.params.id}`
+  })
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(
   PORT, 
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`
-  ))
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
