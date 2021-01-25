b2906ef58c981fd0d6dbb84bf28c83ebfa6f271a
Tony Lee <hy.tony.lee@gmail.com>
2021-01-24 19:41:51 -0800

    setup connection env to mongoDb

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config({ path: './config/config.env'});
+
+// Connect to Database
+connectDB();
 
 // Route files
 const balloons = require('./routes/balloons');
-
-//Load env vars
-dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
@@ -15,6 +19,8 @@ if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
   // app.use(logger);
 }
+
+
 
 
 // Mount Router
